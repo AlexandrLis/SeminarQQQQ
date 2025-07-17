30ca4ba6cc38a3f52c9f54fcd9bb8ef507d44e42
AlexandrLis <Sanya_1997_mai@mail.ru>
2025-07-17 18:08:22 +0300

    Initial commit

---
+++ src/main/java/pack/Main.java
@@ -0,0 +1,40 @@
+package pack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+            func(5);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Код после блока try catch");
+
+    }
+
+    public static void func(int a){
+
+        try{
+            func(a);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
